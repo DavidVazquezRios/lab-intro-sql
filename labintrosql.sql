8b5a4ac6f06823d7585e1ca1417b7c27415aebae
David Vazquez <davidvazquezrios@gmail.com>
2022-01-29 17:32:34 +0100

    labintrosql

---
+++ labintrosql.sql
@@ -0,0 +1,14 @@
+-- Use sakila database.
+use sakila;
+-- Get all the data from tables actor, film and customer.
+select * from actor, film, customer;
+-- Get film titles.
+select title from film;
+-- Get unique list of film languages under the alias language. 
+select distinct name as language from language;
+--  Find out how many stores does the company have?
+select distinct count(store_id) from store;
+-- Find out how many employees staff does the company have?
+select distinct count(staff_id) from staff;
+-- Return a list of employee first names only
+select first_name from staff;
